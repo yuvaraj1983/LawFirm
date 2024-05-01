e22a783f3f1fb74b0616754f230b34553cf3db50
yuvaraj <yuvaraj@coursera-something.com>
2024-05-01 22:33:00 +0530

    Navbar completed

---
+++ lawfirmnew/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { AppBar, Toolbar, Typography, Button, IconButton, Box } from '@mui/material'
+import Logo from '../../assets/Logo.png'
+
+const Navbar = () => {
+
+    const hdr = [
+        {name: "Home"},
+        {name: "Attorneys"},
+        {name: "Practice Areas"},
+        {name: "About Us"}
+    ]
+  return (
+    <AppBar sx={{backgroundColor:'black', display:'flex', flexDirection:'column',
+     justifyContent:'space-between', textTransform: 'none', fontFamily: "Poppins", }}>
+        <Toolbar >
+        <img src={Logo} alt="no Logo found" />
+           
+            <Box sx={{marginLeft:'auto'}}>
+           
+                {
+                    hdr.map((header) =>(
+                        <Button sx={{color:'white'}}>{header.name}</Button>
+                    ))
+                }
+                
+            </Box>
+            
+            <Button sx={{marginLeft:'auto', color:'white', border: 1}} >Contact Now</Button>
+        </Toolbar>
+       
+    </AppBar>
+  )
+}
+
+export default Navbar
